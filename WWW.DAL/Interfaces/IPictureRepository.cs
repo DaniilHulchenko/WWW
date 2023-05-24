e0ec7995b1c96180ca05977627dc14ef35bb0f7b
bardavkov <91415386+bardavkov@users.noreply.github.com>
2023-05-24 20:14:22 +0300

    Update IPictureRepository.cs

--- WWW.DAL/Interfaces/IPictureRepository.cs
+++ WWW.DAL/Interfaces/IPictureRepository.cs
@@ -7,7 +7,7 @@ using WWW.Domain.Entity;
 
 namespace WWW.DAL.Interfaces
 {
-    public interface IPictureRepository:IBaseRepository<Picture>
+    public interface IPictureRepository:IBaseRepository<Picture>// видалити
     {
     }
 }
